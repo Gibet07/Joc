89329d9df9ed887a4c97a1ef3c36f4d23fd9015c
Gibet07 <gdominguez20060@iesjoanramis.org>
2025-02-07 09:56:11 +0100

    Add files via upload

--- Joc/index.js
+++ Joc/index.js
@@ -4,6 +4,35 @@ const ctx = canvas.getContext("2d")
 canvas.height = 512;
 canvas.width = 448;
 
+//VARIABLES DES MURS
+const files = 6
+const columnes = 12
+const ampleMur = 30;
+const alturaMur = 14;
+const margeTMur = 80;
+const margeEMur = 30;
+const separacioMur = 2;
+
+const murs = []
+const ESTAT_MUR = {
+    DESTRUIT: 0,
+    SHOW: 1
+}
+
+for(let c=0; c<columnes; c++){
+    murs[c]=[];
+    for(let f=0; f<files; f++){
+        const murX = margeEMur+c*(ampleMur+separacioMur)
+        const murY = margeTMur+f*(alturaMur+separacioMur)
+        murs[c][f] ={
+            x: murX,
+            y: murY,
+            status: ESTAT_MUR.SHOW,
+            color: "#FFF"
+        } 
+    }
+}
+
 //VARIABLES PILOTA
 let radiPilota = 4;
 let x = canvas.width / 2
@@ -22,7 +51,9 @@ let dreta = false;
 let esquerra = false;
 
 let palaX = (canvas.width- amplePala) / 2
-let palaY = canvas.height - alturaPala - 10
+let palaY = canvas.height - alturaPala - 1
+
+let vidas = 3
 
 
 function pintarPilota(){
@@ -37,7 +68,17 @@ function pintarPala(){
     ctx.fillRect(palaX,palaY,amplePala,alturaPala)
 }
 function pintarMurs(){
-
+ for(let c=0; c<columnes;c++){
+    for (let f=0; f<files; f++){
+        const murActual = murs[c][f];
+        if(murActual.status == ESTAT_MUR.DESTRUIT){
+            continue;
+        }
+        ctx.fillStyle = murActual.color;
+        ctx.rect(murActual.x,murActual.y,ampleMur,alturaMur)
+        ctx.fill();
+    }
+ }
 }
 function deteccioColisio(){
 
@@ -51,15 +92,30 @@ function movimentPilota(){
     if(y + dy <= 0){
         dy=-dy
     }
+
+    const mateixaX = x > palaX && x < palaX + amplePala;
+    const mateixaY = y + dy > palaY 
     //GAME OVER
-    if(y + dy > canvas.height ){
+    if(mateixaX && mateixaY){
+        dy = -dy
+    }
+    else if(y + dy > canvas.height ){
+        vidas--
+
+        x = canvas.width / 2
+        y = canvas.height - 30
+        dx = 2
+        dy = -2
+
+            if(vidas==0){
         console.log("GAME OVER")
         document.location.reload();
-    }
+    }}
     //Moviment
     x += dx
     y += dy
-}
+
+    }
 function movimentPala(){
     if(dreta && palaX < canvas.width - amplePala){
         palaX += sensibilitat
@@ -105,7 +161,19 @@ function inicialitzadorEvents(){
         if(event.key == 'o'){
             dx = dx / 2; dy = dy / 2
         }
-    
+        if(event.key == 't'){
+            let dxNova = dx;
+            let dyNova = dy;
+
+            dx = 0
+            dy = 0
+            setTimeout(()=>{
+                dx = dxNova;
+                dy = dyNova;
+            },3000)
+
+        }
+       
     }
 
     function soltar(event){
@@ -130,6 +198,8 @@ function pintarCanvas(){
     deteccioColisio();
     movimentPilota();
     movimentPala();
+
+    ctx.fillText("VIDAS:"+vidas, canvas.width / 2, 10);
 
     window.requestAnimationFrame(pintarCanvas);
 }
